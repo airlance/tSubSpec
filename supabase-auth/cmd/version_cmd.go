6ca15f497428e1180994f1ecae585b58dad2a7ad
Airlance <93010324+airlance@users.noreply.github.com>
2025-11-08 00:27:10 +0200

    Add files via upload

---
+++ supabase-auth/cmd/version_cmd.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/airlance/api/internal/utilities"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = cobra.Command{
+	Run: showVersion,
+	Use: "version",
+}
+
+func showVersion(cmd *cobra.Command, args []string) {
+	fmt.Println(utilities.Version)
+}
